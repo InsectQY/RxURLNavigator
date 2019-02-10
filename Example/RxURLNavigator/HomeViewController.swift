a08ea1b642ff774f7b7eae7d379d2076d882b895
InsectQY <2562249572@qq.com>
2019-02-11 00:05:54 +0800

    Update Demo

--- Example/RxURLNavigator/HomeViewController.swift
+++ Example/RxURLNavigator/HomeViewController.swift
@@ -18,23 +18,23 @@ class HomeViewController: BaseViewController {
 
     // MARK: - Lazyload
     private lazy var tableView: UITableView = {
-        
+
         let tableView = UITableView(frame: UIScreen.main.bounds)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: id)
         return tableView
     }()
-    
+
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.addSubview(tableView)
-        
+
         let routerType: [RouterType] = [.pushWithBinder, .presentWithBinder, .pushWithSubscribe, .presentWithSubscribe]
-        
+
         Observable.just(routerType)
         .bind(to: tableView.rx.items(cellIdentifier: id, cellType: UITableViewCell.self)) { (row, elememt, cell) in
-            
+
             cell.textLabel?.text = elememt.rawValue
             cell.accessoryType = .disclosureIndicator
             cell.selectionStyle = .none
@@ -43,18 +43,16 @@ class HomeViewController: BaseViewController {
         // Push With Subscribe
         tableView.rx.modelSelected(RouterType.self)
         .filter { $0 == .pushWithSubscribe }
-        .map { _ in navigator.rx.push(UserURL.login.path) }
-        .subscribe { _ in
-
-        }.disposed(by: disposeBag)
+        .flatMap { _ in navigator.rx.push(UserURL.login.path) }
+        .subscribe { _ in }
+        .disposed(by: disposeBag)
 
         // Present With Subscribe
         tableView.rx.modelSelected(RouterType.self)
         .filter { $0 == .presentWithSubscribe }
-        .map { _ in navigator.rx.present(UserURL.login.path, wrap: UINavigationController.self) }
-        .subscribe { 
-            print($0)
-        }.disposed(by: disposeBag)
+        .flatMap { _ in navigator.rx.present(UserURL.login.path, wrap: UINavigationController.self) }
+        .subscribe { _ in }
+        .disposed(by: disposeBag)
 
         /// Also you can use Binder
 
@@ -64,7 +62,7 @@ class HomeViewController: BaseViewController {
         .wrapPush(navigator, UserURL.login.path)
         .bind(to: navigator.rx.push)
         .disposed(by: disposeBag)
-        
+
         // Present With Binder
         tableView.rx.modelSelected(RouterType.self)
         .filter { $0 == .presentWithBinder }
